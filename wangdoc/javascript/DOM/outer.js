94d9894df7b4d6f2cb549c4a8923d8aa0a4f93ee
xiaogang <xyg843978760@163.com>
2023-01-30 08:37:39 +0800

    练习

---
+++ wangdoc/javascript/DOM/outer.js
@@ -0,0 +1,7 @@
+
+
+console.log('-----outer js');
+
+// console.log(document.currentScript);
+// console.log(document.currentScript.outerHTML, '---sss');
+// console.log(document.currentScript === document.getElementById('outer')); // true
