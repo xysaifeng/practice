4ecab2c54292f1fa3731d692acce143638d977f9
xiaogang <xyg843978760@163.com>
2023-06-06 08:45:31 +0800

    submit demo

---
+++ documnt/DOMContentLoaded/index.js
@@ -0,0 +1,11 @@
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   console.log('DOMContentLoaded', el, getComputedStyle(el).color, 2);
+// })
+
+// window.addEventListener('load', function () {
+//   console.log('load',);
+// })
+
+console.log('---------外部脚本执行了');
